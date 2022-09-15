620f96fb04b006af984e1d980eb8dc0edab1af51
liangshuang15 <liangshuang15@macdeMacBook-Pro.local>
2022-09-15 15:10:23 +0800

    init

---
+++ manage-tool/src/components/home.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Anthor: liangshuang15
+ * @Description:
+ * @Date: 2022-09-15 15:03:38
+ * @LastEditTime: 2022-09-15 15:08:37
+ * @FilePath: /wbmanageTool/manage-tool/src/components/Home.vue
+-->
+<template>
+  <div>
+    33333444
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeComponent',
+  props: {
+  }
+}
+</script>
