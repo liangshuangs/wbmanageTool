904584a5f9c0188dc3366105e9ea060b81b8ae51
liangshuang15 <liangshuang15@192.168.1.6>
2022-10-15 23:28:24 +0800

    d

--- manage-tool/public/host.js
+++ manage-tool/public/host.js
@@ -2,7 +2,8 @@
  * @Anthor: liangshuang15
  * @Description: 
  * @Date: 2022-10-15 13:49:33
- * @LastEditTime: 2022-10-15 13:49:56
+ * @LastEditTime: 2022-10-15 21:38:12
  * @FilePath: /wbmanageTool/manage-tool/public/host.js
  */
-window.myHost = 'http://123.60.52.23:3001';
+// window.myHost = 'http://192.168.3.164:3001';
+window.myHost = 'http://123.60.52.23:3001'
